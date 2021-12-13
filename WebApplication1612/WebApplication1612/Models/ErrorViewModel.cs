640483325156fd9f5048eb48d89c1081d8ed33d7
DuyQuang1122 <77633978+DuyQuang1122@users.noreply.github.com>
2021-12-13 22:24:11 +0700

    .

---
+++ WebApplication1612/WebApplication1612/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplication1612.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
